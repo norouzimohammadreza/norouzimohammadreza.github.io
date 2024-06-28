3953241b86f867dd99ff11661e75aab71cd5f12e
Mohammadreza Norouzi <rda0044@gmail.com>
2024-06-28 23:30:48 +0330

    add typing effect

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-// Add custom JavaScript here
+function runTypingEffect(){
+    const text = 'Im Reza Norouzi';
+    const typingElement = document.getElementById('typing-text'); 
+    const delay = 100;
+
+    typeText(text,typingElement,delay);
+
+}
+function typeText(text,typingElement,delay){
+    for (let i = 0; i < text.length; i++) {
+        setTimeout(()=>{
+            typingElement.textContent += text.charAt(i);
+        },delay*i);
+    }
+}
+
+document.addEventListener('DOMContentLoaded',runTypingEffect);
